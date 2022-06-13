c76b6c32287877d8165416bf93f642dcfa7a4cdd
kaanyilmazer <iamyilmazer@gmail.com>
2022-06-13 22:18:17 +0300

    Flutter/YapılacaklarUygulamasi/TodoApp

---
+++ android/app/src/main/kotlin/com/example/todoapp01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todoapp01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/anasayfa.dart
@@ -0,0 +1,138 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'gorevekle.dart';
+import 'package:intl/intl.dart';
+
+class AnaSayfa extends StatefulWidget {
+  const AnaSayfa({Key? key}) : super(key: key);
+
+  @override
+  State<AnaSayfa> createState() => _AnaSayfaState();
+}
+
+class _AnaSayfaState extends State<AnaSayfa> {
+
+  String? mevcutkullaniciUidTutucu;
+
+  @override
+  void initState() {
+    mevcutkullaniciUidsiAl();
+   super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Yapılacaklar Listesi"),
+        actions: [
+          IconButton(
+              onPressed:() async{
+                await FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(Icons.exit_to_app))
+        ],
+      ) ,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection("Gorevler")
+          .doc(mevcutkullaniciUidTutucu).collection("Gorevlerim").snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> veriTabanindanGelenVeriler) {
+            if(veriTabanindanGelenVeriler.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              final alinanVeri = veriTabanindanGelenVeriler.data!.docs;
+              return ListView.builder(
+                itemCount: veriTabanindanGelenVeriler.data!.docs.length,
+                itemBuilder: (context,index){
+                  var eklemeZamani = (alinanVeri[index]['tamZaman'] as Timestamp).toDate();
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(10, 5, 10, 3),
+                    height:  90,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Flexible(
+                                      child: Text(alinanVeri[index]['ad'],
+                                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)),
+                                  SizedBox(
+                                    height: 7,
+                                  ),
+                    Text(DateFormat.yMd().add_jm().format(eklemeZamani).toString(),
+                      style: TextStyle(fontSize: 24),
+                                  )
+                                ],
+                              )),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(alinanVeri[index][index]['sonTarih'],
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
+                              ),)
+                            ],
+                          ),
+                          IconButton(
+                              onPressed: () async {
+                                await FirebaseFirestore.instance
+                                    .collection('Gorevler')
+                                    .doc(mevcutkullaniciUidTutucu)
+                                    .collection('Gorevlerim')
+                                    .doc(alinanVeri[index]['zaman'],)
+                                    .delete();
+                              },
+                          icon: Icon(Icons.delete)
+                            ,)
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.blue,
+        ),
+          backgroundColor: Colors.red,
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (_) => GorevEkle()));
+      },
+        ),
+        );
+
+
+  }
+   
+
+  void mevcutkullaniciUidsiAl() async {
+    FirebaseAuth yetki = FirebaseAuth.instance;
+    var mevcutKullanici = await yetki.currentUser;
+    setState(() {
+      mevcutkullaniciUidTutucu = mevcutKullanici!.uid;
+
+  });
+  }
+}
